bf20b385245c5539df4ed7c4d50f9e6a58806602
차동민 <cardongmin@gmail.com>
2016-04-03 17:02:04 +0900

    OpenCVsharp 2.4.10

---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading; 
+
+
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+using OpenCvSharp.Extensions;
+using OpenCvSharp.UserInterface;
+using OpenCvSharp.Utilities;
+
+namespace Cam
+{
+    public partial class Form1 : Form
+    {
+        CvCapture capture;
+        IplImage imgSrc;
+        TestSURF tsurf;
+
+
+        public bool isComputing;                           //지금 추출 중인가?
+        OpenCvSharp.CPlusPlus.Point[] rectanPts;    //추출된 영역. 4개의 Point로 구성되어 있다.
+        Thread ComputingSURF;                       //계산을 하는 스레드.
+
+        public int capture_type;
+        static int DO_WITH_DIRECT_WEBCAM = 0;       //PC에 직접연결된 웹캠
+        static int DO_WITH_IP_CAMERA = 1;           //IP 웹캠(안드로이드도 된다!)
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            isComputing = false;
+            //capture_type = DO_WITH_IP_CAMERA;           //웹캠 or IP웹캠
+            capture_type = DO_WITH_DIRECT_WEBCAM;
+
+            rectanPts = new OpenCvSharp.CPlusPlus.Point[4];
+            //init
+            for (int i = 0; i < 4; i++ )
+                rectanPts[i].X = rectanPts[i].Y = 0;
+            
+
+            //TestSIFT는 SURF+kdTreeMatching+RANSAC 을 구현하여 영역을 추출한다.
+            tsurf = new TestSURF();
+            tsurf.setTarget("origim.png");      //검색하려는 이미지 파일명
+            
+            
+            
+        }
+
+        //Capture a image from webcam in Timer
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            /*
+            if (capture_type == DO_WITH_IP_CAMERA)
+            {
+                //capture = CvCapture.FromFile("[IP CAMERA ADDR]");
+                capture.SetCaptureProperty(CaptureProperty.FrameWidth, 680);
+                capture.SetCaptureProperty(CaptureProperty.FrameHeight, 480);
+            }
+             */
+          
+
+            //Retrieve image as Iplimage to resize
+            //이미지를 IplImage 타입으로 추출하여 가져온 후, resize한 이미지를 사용
+            IplImage temimg = new IplImage(680, 480, BitDepth.U8, 3);
+            temimg = capture.QueryFrame();
+
+            if (temimg == null) return;
+
+            imgSrc = Cv.CreateImage(new CvSize(680, 480), BitDepth.U8, 3);
+            Cv.Resize(temimg, imgSrc, Interpolation.Linear);
+            Cv.ReleaseImage(temimg);
+
+            //Object Detection will be implmented by running thread.
+            //If thread is running already, this procedure is to be skipped.
+            //[This allows user see smooth webcam streaming]
+            //영역 계산 중이라면 스킵. 계산 중이 아니라면 영역 계산 스레드를 실행
+            if (!isComputing)
+            {
+                isComputing = true;
+
+                ComputingSURF = new Thread(() => TestSURF.setImg(tsurf, this, imgSrc));
+                ComputingSURF.Start();
+                
+            }
+            
+            
+            //Draw Object Rectangle with cvLine
+            //가지고 있는 영역 값을 이용해 매 영상마다 영역은 항상 표시하도록 한다.
+            drawRec(rectanPts);
+            pictureBoxIpl1.ImageIpl = imgSrc;
+
+        }
+
+        //Set object rectangle, this method is to be called in thread
+        //가지고 있는 영역 값을 설정한다. 스레드에서 이 함수를 호출하여, 계산이 끝날 때마다 업데이트 시킨다.
+        public void setDetectionRec(int u0, int v0, int u1, int v1, int u2, int v2, int u3, int v3)
+        {
+            rectanPts[0] = new OpenCvSharp.CPlusPlus.Point(u0, v0);
+            rectanPts[1] = new OpenCvSharp.CPlusPlus.Point(u1, v1);
+            rectanPts[2] = new OpenCvSharp.CPlusPlus.Point(u2, v2);
+            rectanPts[3] = new OpenCvSharp.CPlusPlus.Point(u3, v3);
+        }
+
+        //draw rectangle with cvLine
+        //가지고 있는 영역 값대로 사각형을 그린다.
+        public void drawRec(OpenCvSharp.CPlusPlus.Point[] pts)
+        {
+            Cv.Line(imgSrc, new CvPoint(pts[0].X, pts[0].Y), new CvPoint(pts[1].X, pts[1].Y), new CvScalar(0, 255, 0), 4);
+            Cv.Line(imgSrc, new CvPoint(pts[1].X, pts[1].Y), new CvPoint(pts[2].X, pts[2].Y), new CvScalar(0, 255, 0), 4);
+            Cv.Line(imgSrc, new CvPoint(pts[2].X, pts[2].Y), new CvPoint(pts[3].X, pts[3].Y), new CvScalar(0, 255, 0), 4);
+            Cv.Line(imgSrc, new CvPoint(pts[3].X, pts[3].Y), new CvPoint(pts[0].X, pts[0].Y), new CvScalar(0, 255, 0), 4);   
+        }
+
+        private void pictureBoxIpl1_Click(object sender, EventArgs e)
+        {
+            if (initCamera())
+            {
+                StartTimer();
+            }
+            else
+            {
+                MessageBox.Show("Cannot detect Camera");
+            }
+        }
+
+        private bool initCamera()
+        {
+            try
+            {
+                if (capture_type == DO_WITH_DIRECT_WEBCAM)
+                {
+                    capture = CvCapture.FromCamera(CaptureDevice.DShow, 0);
+                    capture.SetCaptureProperty(CaptureProperty.FrameWidth, 680);
+                    capture.SetCaptureProperty(CaptureProperty.FrameHeight, 480);
+
+                    return true;
+                }
+
+                if (capture_type == DO_WITH_IP_CAMERA)
+                {
+                    //capture = CvCapture.FromFile("http://192.168.0.5:8080/shot.jpg");
+                    //capture.SetCaptureProperty(CaptureProperty.FrameWidth, 680);
+                    //capture.SetCaptureProperty(CaptureProperty.FrameHeight, 480);
+
+                    return true;
+                }
+                    
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private void StartTimer()
+        {
+            timer1.Interval = 2;
+            timer1.Enabled = true;
+        }
+        
+    }
+}
---
+++ TestSURF.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+/*
+using Emgu.Util.TypeEnum;
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Util;
+using Emgu.CV.Flann;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Features2D;
+*/
+
+
+
+using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+using OpenCvSharp.Extensions;
+using OpenCvSharp.UserInterface;
+using OpenCvSharp.Utilities;
+
+ 
+namespace Cam
+{
+    public class TestSURF
+    {
+        public Mat target;             //target Object
+        public bool isComputing;       //bool var for running only single thread at once
+
+
+        public SURF surfobj;             //SURF class
+        public FlannBasedMatcher fm;     //MATCHER
+
+        //---target image's keypoints and descriptor
+        public KeyPoint[] t_keypoints;
+        public Mat t_descriptor;
+        Point2d[] obj_corners = new Point2d[4];
+
+        public TestSURF()
+        {
+            //Constructor
+            //생성자에서 필요한 배열들을 선언
+
+            t_keypoints = new KeyPoint[10000];
+            t_descriptor = new Mat();
+
+            surfobj = new SURF();
+            surfobj.HessianThreshold = 400;
+            
+            
+            fm = new FlannBasedMatcher();
+            isComputing = false;
+
+        }
+
+        //Set target image to retrieve keypoints and descriptor(SURF)
+        //Target 등록하여 디스크립터 추출
+        public void setTarget(string filename)
+        {
+            
+            target = Cv2.ImRead(filename, LoadMode.GrayScale);
+
+            t_keypoints = surfobj.Detect(target);                   //SURF keypoint
+            surfobj.Compute(target, ref t_keypoints, t_descriptor); //SURF descriptor
+            obj_corners = new Point2d[4];
+
+            //target의 크기에 맞는 사각형 (For RANSAC)
+            obj_corners[0] = new Point2d(0, 0);
+            obj_corners[1] = new Point2d(target.Cols, 0);
+            obj_corners[2] = new Point2d(target.Cols, target.Rows);
+            obj_corners[3] = new Point2d(0, target.Rows);
+
+        }
+
+        //Set Frame image from Webcam to start matching&detection
+        //img에 Mat 하나를 등록. 본격적인 매칭을 시작하도록 한다
+        public static void setImg(TestSURF thisobj, Form1 mainform, IplImage imgFromCam)
+        {
+            if (thisobj.isComputing) return;
+
+
+            thisobj.isComputing = true;
+
+            //---frame 특징점, 디스크립터
+            //---frame image's keypoints and descriptor
+            KeyPoint[] f_keypoints;
+            Mat f_descriptor;
+
+            Mat imgOrig;            //캠 영상 (Original)
+            Mat img;                //캠 영상 (Grayscale)
+
+            //Convert to GrayScale Mat
+            imgOrig = Cv2.CvArrToMat(imgFromCam);
+            img = new Mat();
+            Cv2.CvtColor(imgOrig, img, ColorConversion.BgrToGray);
+
+
+
+            //---------------------1. 디스크립터 추출 (keypoint & descriptor retrieval)
+            f_keypoints = new KeyPoint[10000];
+            f_descriptor = new Mat();
+
+            f_keypoints = thisobj.surfobj.Detect(img);                      //SIFT keypoint
+            thisobj.surfobj.Compute(img, ref f_keypoints, f_descriptor);    //SIFT descriptor
+
+
+
+
+            //---------------------2. 매칭 (descriptor Matching)
+            DMatch[] matches = new DMatch[10000];
+
+            try
+            {
+                matches = thisobj.fm.Match(thisobj.t_descriptor, f_descriptor);          //MATCHING
+                
+                //matching error will be caught in this block
+            }
+            catch { return; }
+
+            
+            //record proper distances for choosing Good Matches
+            //좋은 매치를 찾기 위해 디스크립터 간 매칭 거리를 기록한다
+            double max_dist = 0;
+            double min_dist = 100;
+
+            for(int i = 0; i < thisobj.t_descriptor.Rows; i++)
+            {
+                double dist = matches[i].Distance;
+
+                if (dist < min_dist) min_dist = dist;
+                if (dist > max_dist) max_dist = dist;
+            }
+
+
+
+            //---------------------3. gootmatch 탐색 (calculating goodMatches)
+            List<DMatch> good_matches = new List<DMatch>();
+
+            for(int i = 0; i < thisobj.t_descriptor.Rows; i++)
+            {
+                if (matches[i].Distance < 3 * min_dist)
+                    good_matches.Add(matches[i]);
+            }
+
+            /*
+            KeyPoint[] goodkey = new KeyPoint[good_matches.Count];
+            for(int goodidx = 0; goodidx < good_matches.Count; goodidx++)
+            {
+                goodkey[goodidx] = new KeyPoint((f_keypoints[good_matches.ElementAt(goodidx).TrainIdx].Pt.X), (f_keypoints[good_matches.ElementAt(goodidx).TrainIdx].Pt.Y), f_keypoints[good_matches.ElementAt(goodidx).TrainIdx].Size);
+            }
+             */
+
+
+            //Goodmatches의 keypoint 중, target과 frame 이미지에 해당하는 keypoint 정리
+            Point2d[] target_lo = new Point2d[good_matches.Count];
+            Point2d[] frame_lo = new Point2d[good_matches.Count];
+
+
+            for(int i = 0; i < good_matches.Count; i++)
+            {
+                target_lo[i] = new Point2d(thisobj.t_keypoints[good_matches.ElementAt(i).QueryIdx].Pt.X,
+                    thisobj.t_keypoints[good_matches.ElementAt(i).QueryIdx].Pt.Y);
+                frame_lo[i] = new Point2d(f_keypoints[good_matches.ElementAt(i).TrainIdx].Pt.X,
+                    f_keypoints[good_matches.ElementAt(i).TrainIdx].Pt.Y);
+            }
+
+
+            //Homography for RANSAC
+            Mat hom = new Mat();
+
+            
+            //-------------------------------4. RANSAC
+            hom = Cv2.FindHomography(target_lo, frame_lo, HomographyMethod.Ransac);
+          
+            Point2d[] frame_corners;
+            frame_corners = Cv2.PerspectiveTransform(thisobj.obj_corners, hom);
+
+
+
+            //Mat -> iplimage
+            //IplImage returnimg = (IplImage)imgOrig;
+
+            mainform.setDetectionRec((int)frame_corners[0].X, (int)frame_corners[0].Y,
+                (int)frame_corners[1].X, (int)frame_corners[1].Y,
+                (int)frame_corners[2].X, (int)frame_corners[2].Y,
+                (int)frame_corners[3].X, (int)frame_corners[3].Y);
+
+            mainform.isComputing = false;
+            thisobj.isComputing = false;
+
+            //Cv2.DrawKeypoints(imgOrig, goodkey, imgOrig);
+            //Cv2.DrawKeypoints(img, f_keypoints, img);
+            //Cv2.ImWrite("temtem.png", img);
+
+            return;
+        }
+
+
+
+
+    }
+}
